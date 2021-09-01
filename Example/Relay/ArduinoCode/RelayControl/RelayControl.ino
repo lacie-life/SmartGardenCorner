34e0987f17a8c59adfd9bf284a44c9605fd262b3
lacie-life <00sao00ios00@gmail.com>
2021-09-01 22:33:18 +0700

    [Update]
    update relay example

--- Example/Relay/ArduinoCode/RelayControl/RelayControl.ino
+++ Example/Relay/ArduinoCode/RelayControl/RelayControl.ino
@@ -1,28 +1,30 @@
 #include "DHT.h"
-#include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x27, 16, 2);
 const int DHTPIN = 8;       
 const int DHTTYPE = DHT11;  
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
   Serial.begin(9600);
-  dht.begin(); 
-//  lcd.init();
-//  lcd.backlight();       
+  dht.begin();
+  pinMode(13, OUTPUT);       
 }
 
 void loop() {
   float h = dht.readHumidity();    
   float t = dht.readTemperature(); 
   
-//  lcd.setCursor(0, 0); 
   Serial.print("Nhiet do: ");
   Serial.println(t);
-//  lcd.setCursor(0, 1);
   Serial.print("Do am:   :");
   Serial.println(h);
 
-  delay(5000);
+  if (t > 30){
+    digitalWrite(13, HIGH);
+  }
+  else{
+    digitalWrite(13, LOW);
+  }
+
+  delay(1000);
 }
